a9f969e77c89c9dd32ff7734bebf6d53d6c470c2
Ömer ÜCEL <omerucel@gmail.com>
2013-04-16 18:04:58 +0300

    Üst menü eklendi ve angular js ile uyumlu hale getirildi.

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,17 +16,57 @@ angular.module('meadowlark', [])
             .otherwise({redirectTo: '/'});
     }]);
 
-function MainController($scope, $route, $routeParams, $location) {
+function MainController($rootScope, $scope, $route, $routeParams, $location) {
     $scope.$route = $route;
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
+
+    // Sayfa başlığı alanı görünme bilgisini tutar.
+    $scope.page_header_visibility = false;
+
+    // Mevcut menü bilgisini tutar.
+    $scope.current_menu = '';
+
+    // Mevcut menü ise active değilse boş değer döndürür.
+    $scope.isCurrentMenu = function(check){
+        return $scope.current_menu == check ? 'active' : '';
+    };
+
+    // Pencere başlığını değiştirir.
+    $scope.$on('setTitle', function(event, title){
+        $rootScope.title = title + ' | Meadowlark';
+    });
+
+    // Sayfa başlığını değiştirir.
+    $scope.$on('setPageHeader', function(event, page_header){
+        $scope.page_header = page_header;
+        $scope.$emit('changeTitle', page_header);
+    });
+
+    $scope.$on('setPageHeaderVisibility', function(event, status){
+        $scope.page_header_visibility = status;
+    });
+
+    // İlgili menüyü seçili hale getirir.
+    $scope.$on('setCurrentMenu', function(event, menu){
+        $scope.current_menu = menu;
+    });
 }
 
 function HomepageController($scope) {
+    $scope.$emit('setPageHeader', 'Anasayfa');
+    $scope.$emit('setCurrentMenu', 'homepage');
+    $scope.$emit('setPageHeaderVisibility', false);
 }
 
 function LoginController($scope) {
+    $scope.$emit('setPageHeader', 'Oturum Aç');
+    $scope.$emit('setCurrentMenu', 'login');
+    $scope.$emit('setPageHeaderVisibility', true);
 }
 
 function RegisterController($scope) {
+    $scope.$emit('setPageHeader', 'Kayıt Ol');
+    $scope.$emit('setCurrentMenu', 'register');
+    $scope.$emit('setPageHeaderVisibility', true);
 }
